b4ad04e5790e84bc74696bfe4d617199626018a0
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-22 17:23:02 -0300

    update

---
+++ java.js
@@ -0,0 +1,14 @@
+const musicas = ["Rap do Madara (Naruto) - ME TORNEI UM DEUS  NERD HITS - 7 Minutoz (youtube).mp3",
+    "Rap do Homem-Aranha - EU TÔ DE VOLTA AO LAR  NERD HITS - 7 Minutoz (youtube).mp3",
+    "ALQUIMISTA - Pablo Matheuz, Gabriel Rodrigues, Lucas A.R.T. e Pedro Alvez [Prod. TheSkyBeats] - 7 Minutoz (youtube).mp3"
+]
+
+let reproduzir = document.createElement("reproduzir")
+
+reproduzir.addEventListener("click",()=>{
+
+    reproduzir = musicas.map((e)=>{
+
+    })
+
+})
