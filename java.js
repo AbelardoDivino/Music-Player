cfc3f8c629117f7e7b611a2838fb66d7d0dd1959
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-23 19:25:11 -0300

    Add CSS functionality to button clicks, update the layout, and find a better cover image using background-img.

--- java.js
+++ java.js
@@ -1,14 +1,59 @@
-const musicas = ["Rap do Madara (Naruto) - ME TORNEI UM DEUS  NERD HITS - 7 Minutoz (youtube).mp3",
-    "Rap do Homem-Aranha - EU TÔ DE VOLTA AO LAR  NERD HITS - 7 Minutoz (youtube).mp3",
-    "ALQUIMISTA - Pablo Matheuz, Gabriel Rodrigues, Lucas A.R.T. e Pedro Alvez [Prod. TheSkyBeats] - 7 Minutoz (youtube).mp3"
-]
 
-let reproduzir = document.createElement("reproduzir")
 
-reproduzir.addEventListener("click",()=>{
+const musicas = 
+[
+    'musicas/1. Lucas A.R.T. - MANGEKYO SHARINGAN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    reproduzir = musicas.map((e)=>{
+    'musicas/2. Lucas A.R.T. - LAÇOS [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    })
+    'musicas/3. Lucas A.R.T. - KAGUYA [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
+    'musicas/4. Lucas A.R.T. - RINNEGAN [Prod. Bonk Beatz] - 7 Minutoz (youtube).mp3',
+
+    'musicas/5. Lucas A.R.T. - SHIPPUDEN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+
+    'musicas/6. Lucas A.R.T. - TSUKUYOMI INFINITO [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+
+    'musicas/7. Lucas A.R.T. - JINCHUURIKI [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+
+    'musicas/8. Lucas A.R.T. - VALE DO FIM [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+
+    'musicas/9. Lucas A.R.T. - KATON [Prod. 808 Ander] - 7 Minutoz (youtube).mp3'
+];
+
+console.log(musicas)
+
+
+let iniciar = document.getElementById("iniciar")
+let voltar = document.getElementById("voltar")
+let reniciar = document.getElementById("reniciar")
+let parar = document.getElementById("parar")
+let proxima = document.getElementById("proxima")
+
+
+iniciar.addEventListener("click",()=>{
+    alert("vou fazer sozinho")
+
+    // tem que pecorrer e fazer uma funçao para o player
+
+    let c
+    for(c = 0 ; c< musicas.length;c++){
+        iniciar.onplay()
+    }
+})
+
+voltar.addEventListener("click",()=>{
+
+})
+
+reniciar.addEventListener("click",()=>{
+
+})
+
+parar.addEventListener("click",()=>{
+    iniciar = null
+})
+
+proxima.addEventListener("click",()=>{
+    
 })
