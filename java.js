f508e27dc0ee50995295b3203825173f61d085f1
AbelardoDivino <aaaaaaaaaaasafafe@gmail.com>
2026-03-24 21:30:11 -0300

    CSS improvements and new JS functionality added / finished fixing containerbar

--- java.js
+++ java.js
@@ -1,59 +1,94 @@
 
 
-const musicas = 
-[
-    'musicas/1. Lucas A.R.T. - MANGEKYO SHARINGAN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+    const musicas = 
+    [
+        'musicas/1. Lucas A.R.T. - MANGEKYO SHARINGAN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/2. Lucas A.R.T. - LAÇOS [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/2. Lucas A.R.T. - LAÇOS [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/3. Lucas A.R.T. - KAGUYA [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/3. Lucas A.R.T. - KAGUYA [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/4. Lucas A.R.T. - RINNEGAN [Prod. Bonk Beatz] - 7 Minutoz (youtube).mp3',
+        'musicas/4. Lucas A.R.T. - RINNEGAN [Prod. Bonk Beatz] - 7 Minutoz (youtube).mp3',
 
-    'musicas/5. Lucas A.R.T. - SHIPPUDEN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/5. Lucas A.R.T. - SHIPPUDEN [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/6. Lucas A.R.T. - TSUKUYOMI INFINITO [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/6. Lucas A.R.T. - TSUKUYOMI INFINITO [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/7. Lucas A.R.T. - JINCHUURIKI [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/7. Lucas A.R.T. - JINCHUURIKI [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/8. Lucas A.R.T. - VALE DO FIM [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
+        'musicas/8. Lucas A.R.T. - VALE DO FIM [Prod. 808 Ander] - 7 Minutoz (youtube).mp3',
 
-    'musicas/9. Lucas A.R.T. - KATON [Prod. 808 Ander] - 7 Minutoz (youtube).mp3'
-];
+        'musicas/9. Lucas A.R.T. - KATON [Prod. 808 Ander] - 7 Minutoz (youtube).mp3'
+    ];
 
-console.log(musicas)
+    console.log(musicas)
+
+    let audio = document.getElementById("audio")
+    let iniciar = document.getElementById("iniciar")
+    let voltar = document.getElementById("voltar")
+    let reniciar = document.getElementById("reniciar")
+    let parar = document.getElementById("parar")
+    let proxima = document.getElementById("proxima")
+    let tempoAtual = document.getElementById("tempoAtual")
+    let barra = document.getElementById("barra")
+    let tempoTotal = document.getElementById("tempoTotal")
+
+    let index = 0
+    audio.src = musicas[index]
+    audio.load(); // 🔥 força recarregar
+    audio.play();
 
 
-let iniciar = document.getElementById("iniciar")
-let voltar = document.getElementById("voltar")
-let reniciar = document.getElementById("reniciar")
-let parar = document.getElementById("parar")
-let proxima = document.getElementById("proxima")
+    audio.addEventListener("timeupdate", () => {
+        let progresso = (audio.currentTime / audio.duration) * 100;
+        barra.value = progresso;
 
+        tempoAtual.innerText = formatarTempo(audio.currentTime);
+    });
 
-iniciar.addEventListener("click",()=>{
-    alert("vou fazer sozinho")
+    audio.addEventListener("loadedmetadata", () => {
+        tempoTotal.innerText = formatarTempo(audio.duration);
+    });
 
-    // tem que pecorrer e fazer uma funçao para o player
+    function formatarTempo(segundos) {
+        let min = Math.floor(segundos / 60);
+        let seg = Math.floor(segundos % 60);
 
-    let c
-    for(c = 0 ; c< musicas.length;c++){
-        iniciar.onplay()
+        if (seg < 10) seg = "0" + seg;
+
+        return min + ":" + seg;
     }
-})
 
-voltar.addEventListener("click",()=>{
+    barra.addEventListener("input", () => {
+        let tempo = (barra.value / 100) * audio.duration;
+        audio.currentTime = tempo;
+    });
 
-})
+    iniciar.addEventListener("click",()=>{
 
-reniciar.addEventListener("click",()=>{
+        audio.play()
 
-})
+    })
 
-parar.addEventListener("click",()=>{
-    iniciar = null
-})
+    parar.addEventListener("click",()=>{
+        audio.pause()
+    })
 
-proxima.addEventListener("click",()=>{
-    
-})
+    voltar.addEventListener("click",()=>{
+        index--
+        if (index < 0) {
+            index = musicas.length - 1;
+        }
+        audio.src = musicas[index]
+        audio.play()
+    })
+
+    proxima.addEventListener("click",()=>{
+        index++
+        if (index >= musicas.length) {
+            index=0
+        }
+        audio.src = musicas[index]
+        audio.play()
+    })
+
